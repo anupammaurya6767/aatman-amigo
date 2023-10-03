42998f510e2e61d7199e3f1fb832d7c9cf8cc316
Anupam Maurya <anupammaurya6767@gmail.com>
2023-10-03 11:40:24 +0530

    Update send_video.py

--- features/send/send_video.py
+++ features/send/send_video.py
@@ -1,37 +1,13 @@
-# send_video.py
-
-import time
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from utils.helpers import find_element_with_retry
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
 
-def send_video(driver, contact_name, video_path):
-    """
-    Send a video to a contact or group on WhatsApp Web.
-
-    :param driver: WebDriver instance
-    :param contact_name: Name of the contact or group
-    :param video_path: Path to the video file to send
-    :return: True if the video is sent successfully, False otherwise
-    """
-    # Find and click on the contact or group
-    contact_element = find_element_with_retry(driver, f"//span[@title='{contact_name}']")
-    if contact_element:
-        contact_element.click()
-
-        # Find the attachment button and click it to open the menu
-        attachment_button = find_element_with_retry(driver, "//div[@title='Attach']")
-        if attachment_button:
-            attachment_button.click()
-
-            # Find and click on the 'Photo & Video Library' option
-            photo_video_option = find_element_with_retry(driver, "//input[@accept='image/*,video/mp4,video/3gpp,video/quicktime']")
-            if photo_video_option:
-                photo_video_option.send_keys(video_path)
-                time.sleep(2)  # Wait for the video to upload
-                send_button = find_element_with_retry(driver, "//span[@data-icon='send']")
-                if send_button:
-                    send_button.click()
-                    return True
-
-    return False
+def send_video(driver, recipient, video_path):
+    driver.get(f'https://www.instagram.com/direct/t/{recipient}/')
+    time.sleep(2)
+    driver.find_element_by_xpath('//input[@accept="video/mp4,video/x-m4v,video/*"]').send_keys(video_path)
+    time.sleep(2)
+    driver.find_element_by_xpath('//button[@type="submit"]').click()
