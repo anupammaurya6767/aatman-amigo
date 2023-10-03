b9f41fe2149964a3daa2bf946dc481adaddf6aee
Anupam Maurya <anupammaurya6767@gmail.com>
2023-10-03 11:52:59 +0530

    Update send_message.py

--- features/send/send_message.py
+++ features/send/send_message.py
@@ -5,10 +5,15 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 
-def insta_send_message(username, message):
-    driver.get(f"https://www.instagram.com/{username}/")
-    time.sleep(2)
-    driver.find_element_by_xpath("//a[contains(@href,'/direct/t/')]").click()
-    time.sleep(2)
-    driver.find_element_by_xpath("//textarea[@placeholder='Message...']").send_keys(message)
-    driver.find_element_by_xpath("//button[contains(@type,'submit')]").click()
+def send_image(driver, recipient, image_path):
+        try:
+            driver.get(f'https://www.instagram.com/direct/t/{recipient}/')
+            time.sleep(2)
+            input_element = driver.find_element_by_xpath('//input[@accept="image/*"]')
+            input_element.send_keys(image_path)
+            time.sleep(2)
+            submit_button = driver.find_element_by_xpath('//button[@type="submit"]')
+            submit_button.click()
+            return "Image sent successfully."
+        except Exception as e:
+            return f"Error sending image: {str(e)}"
