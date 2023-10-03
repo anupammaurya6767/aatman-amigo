2d4a90e1fc8b50317883bc4aa4549947f4c99624
Anupam Maurya <anupammaurya6767@gmail.com>
2023-10-03 11:56:14 +0530

    Update send_stickers.py

--- features/send/send_stickers.py
+++ features/send/send_stickers.py
@@ -5,14 +5,18 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 
-def send_sticker(driver, recipient, sticker_url):
-    driver.get(f'https://www.instagram.com/direct/t/{recipient}/')
-    time.sleep(2)
-    driver.execute_script(f'window.open("{sticker_url}", "_blank");')
-    time.sleep(2)
-    driver.switch_to.window(driver.window_handles[1])
-    time.sleep(2)
-    driver.find_element_by_xpath('//button[@type="submit"]').click()
-    time.sleep(2)
-    driver.close()
-    driver.switch_to.window(driver.window_handles[0])
+ def send_sticker(driver, recipient, sticker_url):
+        try:
+            driver.get(f'https://www.instagram.com/direct/t/{recipient}/')
+            time.sleep(2)
+            driver.execute_script(f'window.open("{sticker_url}", "_blank");')
+            time.sleep(2)
+            self.driver.switch_to.window(self.driver.window_handles[1])
+            time.sleep(2)
+            submit_button = driver.find_element_by_xpath('//button[@type="submit"]')
+            submit_button.click()
+            driver.close()
+            self.driver.switch_to.window(self.driver.window_handles[0])
+            return "Sticker sent successfully."
+        except Exception as e:
+            return f"Error sending sticker: {str(e)}"
