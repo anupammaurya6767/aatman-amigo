4bdcfde12805edce60d1515717e3916a4d72e87a
Anupam Maurya <anupammaurya6767@gmail.com>
2023-10-03 11:53:58 +0530

    Update send_image.py

--- features/send/send_image.py
+++ features/send/send_image.py
@@ -6,8 +6,14 @@ from selenium.webdriver.common.by import By
 import time
 
 def send_image(driver, recipient, image_path):
-    driver.get(f'https://www.instagram.com/direct/t/{recipient}/')
-    time.sleep(2)
-    driver.find_element_by_xpath('//input[@accept="image/*"]').send_keys(image_path)
-    time.sleep(2)
-    driver.find_element_by_xpath('//button[@type="submit"]').click()
+        try:
+            driver.get(f'https://www.instagram.com/direct/t/{recipient}/')
+            time.sleep(2)
+            input_element = driver.find_element_by_xpath('//input[@accept="image/*"]')
+            input_element.send_keys(image_path)
+            time.sleep(2)
+            submit_button = driver.find_element_by_xpath('//button[@type="submit"]')
+            submit_button.click()
+            return "Image sent successfully."
+        except Exception as e:
+            return f"Error sending image: {str(e)}"
