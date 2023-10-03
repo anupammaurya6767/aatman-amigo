7bc93b3ca3ec244022a138116195ef196f299103
Anupam Maurya <anupammaurya6767@gmail.com>
2023-10-03 11:56:49 +0530

    Update send_video.py

--- features/send/send_video.py
+++ features/send/send_video.py
@@ -6,8 +6,14 @@ from selenium.webdriver.common.by import By
 import time
 
 def send_video(driver, recipient, video_path):
-    driver.get(f'https://www.instagram.com/direct/t/{recipient}/')
-    time.sleep(2)
-    driver.find_element_by_xpath('//input[@accept="video/mp4,video/x-m4v,video/*"]').send_keys(video_path)
-    time.sleep(2)
-    driver.find_element_by_xpath('//button[@type="submit"]').click()
+        try:
+            driver.get(f'https://www.instagram.com/direct/t/{recipient}/')
+            time.sleep(2)
+            video_input = driver.find_element_by_xpath('//input[@accept="video/mp4,video/x-m4v,video/*"]')
+            video_input.send_keys(video_path)
+            time.sleep(2)
+            submit_button = driver.find_element_by_xpath('//button[@type="submit"]')
+            submit_button.click()
+            return "Video sent successfully."
+        except Exception as e:
+            return f"Error sending video: {str(e)}"
