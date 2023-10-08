53d312994715fd8d377707063928e21e068bdba0
Anupam Maurya <anupammaurya6767@gmail.com>
2023-10-08 20:02:00 +0530

    Update insta_api.py

--- api/insta_api.py
+++ api/insta_api.py
@@ -2,11 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from features.login.login_insta import login
 from features.send.send_message import send_message
+from utils.constants import DRIVER_PATH
 
 
 class AatmanAmigo:
     def __init__(self):
-        self.driver = webdriver.Chrome(executable_path="driver_path")
+        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH)
 
     def login(self, username, password):
         
